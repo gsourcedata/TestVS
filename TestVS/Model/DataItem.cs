c92d7a3529d711ad1b0da0a6cd66aca3939fa720
Vikas <Vikas@Gsource271.gsource.com>
2021-07-28 14:23:34 +0530

    first commit

---
+++ TestVS/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace TestVS.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
